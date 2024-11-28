0ff30420658094295a49b14061b08082b66781cb
CarlesVicedo <carlesvicedo@gmail.com>
2024-11-28 19:38:38 +0100

    Save registers in database

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,15 @@
 import {Router} from 'express'
+import User from './models/User'
 
 const router = Router()
 
 // Autentication and sign in
-router.post('/auth/register', (req, res) => {
-    console.log(req.body)
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body)
+
+    await user.save()
+
+    res.send('Register was created')
 })
 
 
