60cd0fc92e84f9db7c6809e5aa9f61633d40cb5b
CarlesVicedo <carlesvicedo@gmail.com>
2024-11-29 11:10:33 +0100

    Create handlers and deal with duplicated registers

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,8 +6,10 @@ export const connectDB = async () => {
         const {connection} = await mongoose.connect(process.env.MONGO_URI)
         const url = `${connection.host}:${connection.port}`
         console.log(colors.cyan.bold(`MongoDB connected at ${url}`))
+
     } catch (error) {
         console.log(colors.bgRed.white.bold(error.message))
         process.exit(1)
+        
     }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,10 @@
-import {Router} from 'express'
-import User from './models/User'
+import { Router } from 'express'
+import { createAccount } from './handlers'
 
 const router = Router()
 
 // Autentication and sign in
-router.post('/auth/register', async (req, res) => {
-    const user = new User(req.body)
-
-    await user.save()
-
-    res.send('Register was created')
-})
+router.post('/auth/register', createAccount)
 
 
 export default router
