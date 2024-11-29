8e573280a188505fd6932dd6db96a390a1187edd
CarlesVicedo <carlesvicedo@gmail.com>
2024-11-29 15:10:21 +0100

    Handle errors and add validations in router

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,18 +1,38 @@
 import {Request, Response} from 'express'
+import { validationResult } from 'express-validator'
 import User from "../models/User"
+import { hashPassword } from '../utils/auth'
+import slug from 'slug'
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const {email} = req.body
+    // Handle errors
+    let errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()})
+        return
+    }
+
+    const {email, password} = req.body
     
     const userExists = await User.findOne({email})
     if (userExists) {
-        const error = new Error('User already exist')
+        const error = new Error('An account with this email already exist')
+       res.status(409).json({error: error.message})
+       return
+    }
+
+    const handle = slug(req.body.handle, '')
+    const handleExists = await User.findOne({handle})
+    if (handleExists) {
+        const error = new Error('Handle already exist')
        res.status(409).json({error: error.message})
        return
     }
 
     const user = new User(req.body)
+    user.password = await hashPassword(password)
+    user.handle = handle
 
     await user.save()
 
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,25 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 // Autentication and sign in
-router.post('/auth/register', createAccount)
+router.post('/auth/register', 
+    body('handle')
+        .notEmpty()
+        .withMessage('Handle can not be empty.'),
+    body('name')
+        .notEmpty()
+        .withMessage('Name can not be empty.'),
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email.'),
+    body('password')
+        .isLength({min: 8})
+        .withMessage('Password must be at least 8 characters long.'),
+
+    createAccount)
 
 
 export default router
