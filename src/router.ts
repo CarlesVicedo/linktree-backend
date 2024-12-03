9a03357170bb2a0716d13e055b99fa496cd70746
CarlesVicedo <vicedocarles@gmail.com>
2024-12-03 22:10:51 +0100

    Add updateUser call and handle

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { body } from 'express-validator'
-import { createAccount, getUser, login } from './handlers'
+import { createAccount, getUser, login, updateProfile } from './handlers'
 import { handleInputErrors } from './middleware/validation'
 import { authenticate } from './middleware/auth'
 
@@ -35,6 +35,17 @@ router.post('/auth/login',
 )
 
 router.get('/user',authenticate, getUser)
+router.patch('/user', 
+        body('handle')
+        .notEmpty()
+        .withMessage('Handle can not be empty.'),
+    body('description')
+        .notEmpty()
+        .withMessage('Description can not be empty.'),
+        handleInputErrors,
+        authenticate, 
+        updateProfile
+)
 
 
 export default router
